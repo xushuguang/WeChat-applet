b350103dc6da1deaa29661d3198cfd9e974b70e7
james.xu <james.xu@idq-qtec.com>
2018-08-28 13:54:40 +0800

    add more

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ App({
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(res.code){
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        }
       }
     })
     // 获取用户信息
